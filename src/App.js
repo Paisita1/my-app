f5320c8b4ebfcd0c1a43f073bb72ee5ea3e93171
Paisita1 <isaacvirgen123@gmail.com>
2020-11-17 19:09:17 -0800

    Started navBar

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ export default class ThriveSolo extends React.Component {
               </NavLink>
             </ul>
           </div>
-          
+          <hr />
+
           <Switch>
             <Route exact path = "/home">
               <Home />
@@ -381,9 +382,41 @@ styles.createAcc = {
 class Blog extends React.Component {
   render() {
     return (
-        <p>yo</p>
+        <div style={{display: "flex", backgroundColor: "black"}}>
+
+          <div style={{display: "flex", marginTop: "27px", width: "100%", borderBottom: "1px solid orange"}}>
+
+            <h2 style={{color: "white"}}>
+              {'CSS-TRICKS'}
+            </h2>
+
+            <div style={{display: "flex", fontFamily: "Ringside Regular A,Ringside Regular B,Rubik,Lato,Lucida Grande,Lucida Sans Unicode,Tahoma,Sans-Serif", fontWeight: "600"}}>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Articles'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Videos'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Almanic'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Newsletter'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Guides'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Books'}</a>
+              <a href="" target="_blank" style={styles.navBarItems}>{'Account'}</a>
+            </div>
+
+          </div>
+
+        </div>
       );
   }
+}
+
+styles.navBarItems = {
+    display: "flex",
+    alignItems: "flex-end",
+    color: "white",
+    textDecoration: "none",
+    margin: "0px 10px",
+    borderRight: "1px solid #201c29",
+    paddingRight: "10px",
+    paddingBottom: "13px",
+    fontSize: "13px"
 }
 
 class Trial extends React.Component {
