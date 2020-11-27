3ab9a11e6b8ce0916c9dfb6239c542e657846652
Paisita1 <isaacvirgen123@gmail.com>
2020-11-27 13:29:15 -0800

    Added flex-grow box

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import container from './divcontainer.svg';
 import items from './divitems.svg';
 import itemsExample1 from './itemsimg1.svg';
 import containerExample1 from './containerimg1.svg';
+import itemsExample2 from './itemsimg2.svg';
 
 const styles = {}
 
@@ -544,6 +545,7 @@ class Blog extends React.Component {
                 <div style={{
                     background: "linear-gradient(rgba(156,39,176,.2),#fff)",
                     padding: "2rem",
+                    marginBottom: "5px"
                   }}
                 >
 
@@ -612,6 +614,7 @@ class Blog extends React.Component {
                 <div style={{
                     background: "linear-gradient(rgba(156,39,176,.2),#fff)",
                     padding: "2rem",
+                    marginBottom: "5px"
                   }}
                 >
 
@@ -778,6 +781,8 @@ class Blog extends React.Component {
 
               </div>
 
+            {/* start of second column */}
+
               <div style={{display: "flex", flexFlow: "column", width: "50%"}}>
 
                 <img src={items} style={{width: "75%"}} />
@@ -801,6 +806,7 @@ class Blog extends React.Component {
                 <div style={{
                     background: "linear-gradient(rgba(255,138,0,.2),#fff)",
                     padding: "2rem",
+                    marginBottom: "5px"
                   }}
                 >
 
@@ -854,6 +860,92 @@ class Blog extends React.Component {
                       </code>
 
                     </div>
+
+                  </div>
+
+                </div>
+
+                <div style={{
+                    background: "linear-gradient(rgba(255,138,0,.2),#fff)",
+                    padding: "2rem",
+                    marginBottom: "5px"
+                  }}
+                >
+
+                  <div>
+
+                    <h2 style={{
+                        margin: "0 0 .8rem",
+                        fontFamily: "Ringside Regular A,Ringside Regular B,Rubik,Lato,Lucida Grande,Lucida Sans Unicode,Tahoma,Sans-Serif",
+                        fontSize: "1.8rem"
+                      }}
+                    >
+                      {'flex-grow'}
+                    </h2>
+
+                    <img src={itemsExample2} alt="img" style={{width: "70%", marginBottom: "50px"}} />
+
+                    <p style={{
+                        fontFamily: "Sentinel SSm A,Sentinel SSm B,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
+                        fontSize: "1.3rem",
+                        margin: "0 0 1.8rem",
+                        width: "95%"
+                      }}
+                    >
+                      {`This defines the ability for a flex item to grow if necessary. 
+                        It accepts a unitless value that serves as a proportion. 
+                        It dictates what amount of the available space inside the flex container the item should take up.`}
+                    </p>
+
+                    <p style={{
+                        fontFamily: "Sentinel SSm A,Sentinel SSm B,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
+                        fontSize: "1.3rem",
+                        margin: "0 0 1.8rem",
+                        width: "95%"
+                      }}
+                    >
+                      {`If all items have`}<code style={{padding: ".1rem .3rem .2rem",backgroundColor: "#fff6ea"}}>flex-grow</code>
+                      {` set to 1, the remaining space in the container will be distributed equally to all children. 
+                        If one of the children has a value of 2, the remaining space would take up twice as much space as the others 
+                        (or it will try to, at least).`}
+                    </p>
+
+                    <div style={{display: "flex", flexFlow: "column", backgroundColor: "#001628", padding: "18px 20px 20px 22px", borderRadius: "8px", marginBottom: "1.5rem"}}>
+                      
+                      <code style={{
+                          color: "#72e0d1",
+                        }}
+                      >
+                        <span style={{color: "#f5d67b"}}>.item</span> {' {'}
+                      </code>
+
+                      <code style={{
+                          color: "#72e0d1",
+                          margin: "10px 0 10px 15px"
+                        }}
+                      >
+                        {'  flex-grow: '} <span style={{color: "#fc9463"}}>4</span>; <span style={{color: "#5e7671", fontStyle: "italic"}}>/* default 0 */</span>
+                      </code>
+
+                      <code style={{
+                          color: "#72e0d1",
+                          margin: "0"
+                        }}
+                      >
+                       {'}'}
+                      </code>
+
+                    </div>
+
+                    <p style={{
+                        fontFamily: "Sentinel SSm A,Sentinel SSm B,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
+                        fontSize: "1.3rem",
+                        margin: "0 0 1.8rem",
+                        width: "95%"
+                      }}
+                    >
+                      {`Negative numbers are invalid.`}
+                    </p>
 
                   </div>
 
